c99c0aae83cfb6ea500bd78820565ecfcc39ff4a
Ayushmaan176823243 <ayushmaanratan@gmail.com>
2026-06-08 13:59:24 +0530

    Updated frontend

--- constant/index.js
+++ constant/index.js
@@ -1,6 +1,5 @@
-// export const API_URL = "http://192.168.29.235:3000";
-export const API_URL = "https://live-ai-image.onrender.com";
+export const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
-// AI-Iamge Generator KEYS
 export const WORKER_URL = "https://image-api.ayushmaanratan.workers.dev/";
-export const WORKER_API_KEY = "asd6d7ad88sdf6678sdfsdfsd86";
+
+export const WORKER_API_KEY = process.env.EXPO_PUBLIC_WORKER_API_KEY;
